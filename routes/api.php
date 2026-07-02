ba2f099b04f025f87f678be91463ad1696c75f0a
Gabriel Pineda <gabrielp2806@gmail.com>
2026-07-02 12:09:01 -0300

    feat: crear api de enrolamiento para el gimnasio

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SensorController;
+use App\Http\Controllers\Api\GymApiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Ruta para recibir la petición del Arduino
+// Ruta para recibir los escaneos del Arduino
 Route::post('/sensor/trigger', [SensorController::class, 'trigger']);
+
+// Rutas de integración HaaS para el Gimnasio
+Route::get('/gym/latest-scan', [GymApiController::class, 'latestScan']);
